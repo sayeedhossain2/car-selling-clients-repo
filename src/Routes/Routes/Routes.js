1840bfcec3dace1ca1af04aadf8fc120c3839b3a
sayeedhossain2 <sayeedh95@gmail.com>
2022-11-28 19:25:08 +0600

    edit

--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -90,6 +90,7 @@ export const router = createBrowserRouter([
         path: "/dashboard/addaproduct",
         element: (
           <SellerRoute>
+            {" "}
             <AddProduct></AddProduct>
           </SellerRoute>
         ),
